3670aea0e1ca9117f65cd07872ef338251925aca
silvia pintus <silviapintus.93@gmail.com>
2021-06-03 16:58:06 +0100

    user pattern added

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let buttonColours = ['red','blue','green','yellow'];
-const gamePattern = [];
+let gamePattern = [];
+let userClickedPattern = [];
 
 const nextSequence = () => {
   let randomNumber = Math.floor(Math.random() * 4);
@@ -7,22 +8,16 @@ const nextSequence = () => {
   gamePattern.push(randomChosenColour);
 
   $('#' + randomChosenColour ).fadeIn(100).fadeOut(100).fadeIn(100);
+  $(document).click(function(event){
+    let userChosenColour = event.target.id
+    userClickedPattern.push(userChosenColour);
+  });
   
   let audio = new Audio('sounds/' + randomChosenColour + '.mp3');
   audio.play();
 
 }
 
-$(document).on("keydown", function(event){
-
-  if (event.key)
-
-  {
-
-    nextSequence();
-
-  }
-
-});
+nextSequence()
 
 
