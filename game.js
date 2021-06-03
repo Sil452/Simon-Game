dd152105b4ac35d9f4fed6a1f6b814313f979c31
silvia pintus <silviapintus.93@gmail.com>
2021-06-03 19:52:44 +0100

    basic game logic

--- game.js
+++ game.js
@@ -1,6 +1,63 @@
 let buttonColours = ['red','blue','green','yellow'];
 let gamePattern = [];
 let userClickedPattern = [];
+let score = -1
+let started = false
+
+$(document).on('keydown', function(event) {
+  // 13 == enter in all browsers
+  if(event.which == 13 && !started){
+    $('#level-title').text('Score: ' + score)
+    nextSequence();
+    userClick()
+    started = true;
+  }
+});
+
+const nextSequence = () => {
+
+  userClickedPattern = [];
+  
+  score++;
+  $('#level-title').text('Score: ' + score)
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+ 
+  changeBgn(randomChosenColour)
+  playSound(randomChosenColour)
+}
+
+function userClick() {
+  $('.btn').click(function(event){
+
+    let userChosenColour = event.target.id
+    userClickedPattern.push(userChosenColour);
+
+    changeBgn(userChosenColour)
+    playSound(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+  });
+}
+
+  function checkAnswer(currentLevel){
+  if(userClickedPattern[currentLevel] == gamePattern[currentLevel]){
+    
+    console.log('yep')
+
+    if (userClickedPattern.length === gamePattern.length){
+
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else{
+    console.log('nope')
+  }
+};
 
 const playSound = (name) => {
   let sound = new Audio('sounds/' + name + '.mp3');
@@ -14,34 +71,3 @@ const changeBgn = (color) => {
     $('#' + color ).css("background-color", 'transparent');
   }, x);
 }
-
-const nextSequence = () => {
-  let randomNumber = Math.floor(Math.random() * 4);
-  let randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
- 
-  changeBgn(randomChosenColour)
-  playSound(randomChosenColour) 
-}
-
-const userclick = () => {
-  $(document).click(function(event){
-    let userChosenColour = event.target.id
-    userClickedPattern.push(userChosenColour);
-    changeBgn(userChosenColour)
-    playSound(userChosenColour);
-  });
-}
-
-$(document).on('keydown', function(event) {
-  // 13 == enter in all browsers
-  if(event.which == 13){
-    $('#level-title').text('Level')
-    nextSequence();
-  }
-})
-
-
-userclick()
-
-
